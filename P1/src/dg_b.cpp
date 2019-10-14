b1076aa035602d6bba66c99e700a82178bcbd824
DiracSea <sulz@mail.sustc.edu.cn>
2019-10-13 23:38:42 -0700

    make

---
+++ P1/src/dg_b.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Author: Longze Su
+ * @Date: 2019-10-13 22:44:02
+ * @Description: CS211_Project1
+ * @LastEditTime: 2019-10-13 22:46:55
+ * @LastEditors: Longze Su
+ */
+
+
+#include "functions.hpp" 
+
+void dgemm4(double* a, double* b, double* c, int n)
+{
+
+}
+void dgemm5(double* a, double* b, double* c, int n)
+{
+    
+}

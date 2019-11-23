4e9c283256ced2828e0b30275be33dd13b722104
DiracSea <sulz@mail.sustc.edu.cn>
2019-11-23 00:13:59 -0800

    elapsedT

--- lab3/sieve3.c
+++ lab3/sieve3.c
@@ -156,7 +156,7 @@ int main (int argc, char *argv[])
    if (p > 1) 
       MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD); 
    
-   elapsedTime += MPI_Wtime();
+   elapsed_time += MPI_Wtime();
    /* Print the results */
 
    if (!id) {
